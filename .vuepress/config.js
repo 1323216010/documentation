b0989c2949b61856a797ce424b1c039432230d77
13232160100 <101637168+1323216010@users.noreply.github.com>
2023-03-07 10:42:31 +0800

    测试

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,6 +3,13 @@ module.exports = {
   title: '我的幻想乡 v1.0',
   description: 'Open source Instant Search Engine',
   theme: 'default-prefers-color-scheme',
+  locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    '/': {
+      lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+    }
+  },
   themeConfig: {
     docsRepo: '我的幻想乡',
     docsBranch: 'main',
